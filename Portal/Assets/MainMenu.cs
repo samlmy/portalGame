24ae8ada2021efabccc3056071c97b351f475882
Mingyang Li <mingyangsamli@gmail.com>
2020-09-19 14:55:08 -0400

    ui
    ui

---
+++ Portal/Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        //Debug.Log("quit");
+        Application.Quit();
+    }
+}
---
+++ Portal/Assets/ScoreKeeper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(MeshCollider collider)
+    {
+        Destroy(collider.gameObject);
+        AddScore();
+    }
+
+    void AddScore()
+    {
+        Score++;
+        ScoreText.text = Score.ToString();
+    }
+}
